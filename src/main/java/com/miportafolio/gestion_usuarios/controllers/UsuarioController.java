43cc4fd83b373537139ee08c638a0b6cb3b73eb1
GabrielBaezB <gabooo@live.com>
2024-11-01 14:51:51 -0300

    se borra texto comentado

--- src/main/java/com/miportafolio/gestion_usuarios/controllers/UsuarioController.java
+++ src/main/java/com/miportafolio/gestion_usuarios/controllers/UsuarioController.java
@@ -11,7 +11,6 @@ import com.miportafolio.gestion_usuarios.services.UsuarioServiceImpl;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
 @RequestMapping("/api/usuarios")
 @CrossOrigin(origins = "http://localhost")
@@ -21,75 +20,46 @@ public class UsuarioController {
     UsuarioServiceImpl usuarioServiceImpl;
 
     @PostMapping
-    public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario){
-        try{
+    public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario) {
+        try {
             Usuario saveUsuario = usuarioServiceImpl.saveUsuario(usuario);
             return new ResponseEntity<>(saveUsuario, HttpStatus.OK);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
     @PutMapping
-    public ResponseEntity<Usuario> updateUsuario(@RequestBody Usuario usuario){
-        try{
+    public ResponseEntity<Usuario> updateUsuario(@RequestBody Usuario usuario) {
+        try {
             Usuario saveUsuario = usuarioServiceImpl.updateUsuario(usuario);
             return new ResponseEntity<>(saveUsuario, HttpStatus.OK);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> getUsuarios(){
+    public ResponseEntity<List<Usuario>> getUsuarios() {
         return new ResponseEntity<>(usuarioServiceImpl.getUsuarios(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getUsuariosById(@PathVariable Long id){
+    public ResponseEntity<Usuario> getUsuariosById(@PathVariable Long id) {
         Optional<Usuario> usuario = usuarioServiceImpl.getUsuariosById(id);
 
-        return usuario.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> 
-                new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        return usuario.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id){
+    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
         Optional<Usuario> usuario = usuarioServiceImpl.getUsuariosById(id);
-        if(usuario.isPresent()){
+        if (usuario.isPresent()) {
             usuarioServiceImpl.deleteUsuario(usuario.get().getId());
             return new ResponseEntity<>(HttpStatus.OK);
-        }else {
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
-
-
-    // private UsuarioService usuarioService;
-
-    // @PostMapping
-    // public Usuario crearUsuario(@RequestBody Usuario usuario){
-    //     return usuarioService.crearUsuario(usuario);
-    // }
-
-    // @GetMapping("/{id}")
-    // public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Long id){
-    //     Optional<Usuario> usuario =  usuarioService.obtenerUsuarioPorId(id);
-    //     return usuario.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    // }
-
-    // @GetMapping
-    // public List<Usuario> obtenerTodosLosUsuarios() {
-    //     return usuarioService.obtenerTodosLosUsuarios();
-    // }
-
-    // @DeleteMapping("/{id}")
-    // public ResponseEntity<Void> eliminarUsuario(@PathVariable Long id) {
-    //     usuarioService.eliminarUsuario(id);
-    //     return ResponseEntity.noContent().build();
-    // }
-    
 }
